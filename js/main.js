7570460c16f899fe8d3cb5af07bdeabc3926f4f9
pabloperez293 <paperezpablo@gmail.com>
2022-08-30 16:45:24 -0300

    Ingreso de pokedexx

--- js/main.js
+++ js/main.js
@@ -7,33 +7,18 @@ function updateUIPokedex(poke) {
 async function getPokemon(id) {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     const data = await res.json()
-    // console.log(data)
-    // con este return va para la fun getPokemon
+    // con return va para la fun getPokemon
     return data
 }
 
 async function init() {
-    let fecha = new Date()
-    let dia = fecha.getDay()
-    let mes = fecha.getMonth()
-    let dia_mes = dia + "-" + mes 
-
-    let num = localStorage.setItem("numero") || 0
-
-    if(num == 0 ) {
-
-        num = Math.ceil(Math.random() * 100)
-        localStorage.setItem("numero", num)
-    }
-    console.log(num)
-    const firtPokemon = await getPokemon(num)
+    const firtPokemon = await getPokemon(4)
     updateUIPokedex(firtPokemon)
-
 }
 
 let search = document.querySelector("#search")
 search.addEventListener("change", async() =>{
-    // Con esto lo busca con id o name .value
+    // Busca con id o name .value
     const pokemon = await getPokemon(search.value)
     updateUIPokedex(pokemon)
 
